6752c718300079f1f861b79075bd3cea44012c53
tymonkilich <tymon.kilich@pg.edu.pl>
2020-04-09 18:41:59 +0200

    Add test and docstring

--- src/svopt.jl
+++ src/svopt.jl
@@ -1,5 +1,8 @@
 module svopt
 
+
+"Function returns squared argument and sum of this argument and 4"
+
 my_f(x) = x^2 + x + 4
 
 export my_f
---
+++ test/runtests.jl
@@ -0,0 +1,5 @@
+using svopt, Test
+
+@test my_f(1) == 6
+@test my_f(3) == 16
+
