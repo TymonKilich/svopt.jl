74642869abc63acb42ce6a0d37aa7603c890fe0d
tymonkilich <tymon.kilich@pg.edu.pl>
2020-04-10 16:22:28 +0200

    Add hill climbing optimization method

--- src/svopt.jl
+++ src/svopt.jl
@@ -1,10 +1,28 @@
 module svopt
 
+"Function optimizes single variable function using primitive hill climbing method"
 
-"Function returns squared argument and sum of this argument and 4"
+function minimhc(f, x0; step=1.0, ϵ=1e-5, dampingfactor=0.5, maxiter=1e6)
+    x, fp = x0, f(x0)
+    s, fs = x0 + step, f(x0 + step)
+    if fs > fp
+        x, s = s, x
+        fp, fs = fs, fp
+        step = -step
+    end
+    i, fn = 0, Inf
+    while abs(fn-fs) ≥ ϵ && i ≤ maxiter
+        i += 1
+        s += step
+        fs = fn
+        fn = f(s)
+        if fn > fs
+            step = -step*dampingfactor
+        end
+    end
+    return s, fn
+end
 
-my_f(x) = x^2 + x + 4
-
-export my_f
+export minimhc
 
 end # module
