cbc2b3b223795a0f89c4a581aa3f88591d40e402
tymonkilich <tymon.kilich@pg.edu.pl>
2020-04-10 16:27:34 +0200

    Fix added tests of hc method

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using svopt, Test
 
 @testset "hillclimb" begin
     x, f = 10, x -> x^4 + 3x^3 + x^2 + sin(x)
-    minimhc(f, x) == (-1.9716796875, -4.915215283984578)
+    @test minimhc(f, x) == (-1.9716796875, -4.915215283984578)
     x, f = 4, x -> x^2
-    minimhc(f, x) == (0.0, 0.0)
+    @test minimhc(f, x) == (0.0, 0.0)
 end
