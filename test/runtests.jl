f73dfd2e98a768fb01eda29b3b24eaa7383ae655
tymonkilich <tymon.kilich@pg.edu.pl>
2020-04-10 16:25:23 +0200

    Add two first test for hc optimization method

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,8 @@
 using svopt, Test
 
-@test my_f(1) == 6
-@test my_f(3) == 16
-
+@testset "hillclimb" begin
+    x, f = 10, x -> x^4 + 3x^3 + x^2 + sin(x)
+    minimhc(f, x) == (-1.9716796875, -4.915215283984578)
+    x, f = 4, x -> x^2
+    minimhc(f, x) == (0.0, 0.0)
+end
